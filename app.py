90218a2781aa7527989b14669b4da35ca7a6ce11
Nidhi Patel <nidhipatel@Nidhis-MBP.fios-router.home>
2018-01-19 18:48:32 -0500

    resetting everyting to the intital commit

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def hello():
     return app.send_static_file('index.html')
 
 
-@app.route('/get_info')
-def getInfo(name):
-    return "Hello " + name
-
-
 if __name__ == "__main__":
     app.run(debug=True)
