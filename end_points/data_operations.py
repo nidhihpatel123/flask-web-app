c7a223942b77a5c3747cedc0ca9b011df9d97cc1
Nidhi Patel <nidhipatel@Nidhis-MBP.fios-router.home>
2018-01-21 19:16:10 -0500

    adding the api to query product page

--- end_points/data_operations.py
+++ end_points/data_operations.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, json
 from elasticsearch_dsl import Search
 from utilities.elasticsearch_connector import get_es_connector
 
@@ -7,11 +7,18 @@ data_operations = Blueprint('data_operations', __name__)
 client = get_es_connector()
 
 
-@data_operations.route("/update")
+@data_operations.route("/update", methods=['POST'])
 def update_data():
-    s = Search()
+    return "Hello Test"
 
 
-@data_operations.route("/search")
+@data_operations.route("/search",methods=['POST'])
 def search_data():
-    s = Search()
+    data = request.get_json(force=True , silent=True)['category']
+    s = Search(using=client, index="products")
+    s = s.query("match", category=data)
+    resp = s.execute().to_dict()['hits']['hits']
+    return_resp = []
+    for r in resp:
+        return_resp.append(r['_source'])
+    return json.dumps(return_resp)
